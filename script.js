d20fd7d7323e64b4d2aa85d5b31199e852240796
ERASTTT <82580195+ERASTTT@users.noreply.github.com>
2022-12-22 13:08:21 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+'use strict';
+//№1
+let a1=7+2, a2=7-3, a3=2*4, a4=4/2;
+console.log(`1)${a1},${a2},${a3},${a4}`)
+ 
+//№2
+let  height=23, width=10, S=width*height;
+console.log(`2)${S}`)
+ 
+//№3
+let b1=5 % 3,b2=3 % 5,b3= 5 + '3',b4='5'-3,b5=75 + 'кг';
+console.log(`3)${b1},${b2},${b3},${b4},${b5}`);
+ 
+//№4
+let heightC=10, dC = 4,P=3.1415926535, V= heightC*P*dC*dC/4;
+console.log(`4)${V}`);
+ 
+//№5
+let z=10, s=2;
+console.log(`5)${z+s},${z-s},${z*s},${z/s}`);
+ 
+//№6
+let k=15, d=2, result= k+d;
+console.log(`6)${result}`);
+ 
+//№7
+let a=10, b=2, c=5;
+document.write(`7)${a+b+c}`);
+ 
+//№8
+let x=17, y=10, j= x-y, r=7, resultat=c+d;
+document.write(`8)${resultat}`)
+ 
+//№9
+let name = "Ilkham"
+console.log('Привет', name);
+ 
+//№10
+let age= "19"
+console.log(`10)${"Мне"},${ age},${"Лет"} `);
+//№11
+let  str = "abcde",t=str[0],p=str[2],q=str[4];
+console.log(`11)${t},${p},${q}`);
+ 
+//№12
+let chas=60*60, syt= 24*chas, math= 30*syt;
+console.log(`12)${chas},${syt},${math}`);
+ 
+//№13
+ let num = 47;
+ num +=  7;
+ num -=  18;
+ num *=  10;
+ num /=  15;
+ console.log(`13)${num}`);
+ 
+ //14
+ var numm = 1;
+ num += 12;
+ num -= 14;
+ num *= 5;
+ num /= 7;
+ num++;
+ num--;
+console.log(`14)${num}`);
+
